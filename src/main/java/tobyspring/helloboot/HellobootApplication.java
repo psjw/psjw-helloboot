4eadabb3a0ec27bb26dcf1423a0626374f88a9d1
psjw <dlrudtn108@gmail.com>
2023-03-28 22:50:41 +0900

    STEP01_ADDServlet

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,46 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+//        ServletWebServerFactory serverFactory = new JettyServletWebServerFactory();
+        //서블릿 생성 -> ServletContextInitializer 인터페이스구현 -> Functional 함수로 사용가능 익명함수를 lambda 변환가능
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() { //1. 서블릿 등록
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    //1. 요청부분
+                    String name = req.getParameter("name");
+
+                    //2. 응답부분
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    //body 생성
+                    resp.getWriter().print("Hello " + name);
+                }
+            }).addMapping("/hello"); //2. 요청에 따른 매핑 URL 추가
+        });
+        webServer.start(); //톰캣 서블릿 컨테이너 동작
     }
 
 }
+
